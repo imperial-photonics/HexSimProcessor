fixed reconfactor calculation when usemodulation.

SIM_processing/hexSimProcessor.py
@@ -184,21 +184,20 @@ class HexSimProcessor:
 
         for idx_p in range(0, 7):
             pstep = idx_p * 2 * pi / 7
-            if self.usemodulation:
-                if useCupy:
-                    self._reconfactor[idx_p, :, :] = (1 + 4 / A[0]  * cp.outer(cp.exp(cp.asarray(1j * (ph * cky[0] * yy - pstep + p[0]))),
-                                                                             cp.exp(cp.asarray(1j * ph * ckx[0] * xx))).real
+            if useCupy:
+                self._reconfactor[idx_p, :, :] = (1 + 4 / A[0]  * cp.outer(cp.exp(cp.asarray(1j * (ph * cky[0] * yy - pstep + p[0]))),
+                                                                           cp.exp(cp.asarray(1j * ph * ckx[0] * xx))).real
                                                   + 4 / A[1] * cp.outer(cp.exp(cp.asarray(1j * (ph * cky[1] * yy - 2 * pstep + p[1]))),
-                                                                           cp.exp(cp.asarray(1j * ph * ckx[1] * xx))).real
+                                                                        cp.exp(cp.asarray(1j * ph * ckx[1] * xx))).real
                                                   + 4 / A[2] * cp.outer(cp.exp(cp.asarray(1j * (ph * cky[2] * yy - 3 * pstep + p[2]))),
-                                                                           cp.exp(cp.asarray(1j * ph * ckx[2] * xx))).real).get()
-                else:
-                    self._reconfactor[idx_p, :, :] = (1 + 4 / A[0]  * np.outer(np.exp(1j * (ph * cky[0] * yy - pstep + p[0])),
+                                                                        cp.exp(cp.asarray(1j * ph * ckx[2] * xx))).real).get()
+            else:
+                self._reconfactor[idx_p, :, :] = (1 + 4 / A[0]  * np.outer(np.exp(1j * (ph * cky[0] * yy - pstep + p[0])),
                                                                            np.exp(1j * ph * ckx[0] * xx)).real
                                                   + 4 / A[1] * np.outer(np.exp(1j * (ph * cky[1] * yy - 2 * pstep + p[1])),
-                                                                           np.exp(1j * ph * ckx[1] * xx)).real
+                                                                        np.exp(1j * ph * ckx[1] * xx)).real
                                                   + 4 / A[2] * np.outer(np.exp(1j * (ph * cky[2] * yy - 3 * pstep + p[2])),
-                                                                           np.exp(1j * ph * ckx[2] * xx)).real)
+                                                                        np.exp(1j * ph * ckx[2] * xx)).real)
 
         # calculate pre-filter factors
 

Tests/hexSimProcessor_test.py
@@ -11,7 +11,7 @@ import time
 from SIM_processing.hexSimProcessor import HexSimProcessor
 
 plt.close('all')
-isPlot = False
+isPlot = True
 N = 10  # number of iterations
 Nsize = 512
 
@@ -57,11 +57,15 @@ try:
 except AssertionError as error:
     print(error)
 
+
+
 ''' Calibration '''
 start_time = time.time()
 h.calibrate(img1)
 elapsed_time = time.time() - start_time
 print(f'Calibration time: {elapsed_time:5f}s ')
+
+# tif.imwrite('test_reconfactor.tif',h._reconfactor)
 
 ''' Recontruction '''
 
@@ -176,7 +180,7 @@ try:
     print(f'CuPy Reconstructframe time: {elapsed_time / (7 * N):5f}s ')
     if isPlot:
         plt.figure()
-        plt.imshow(imgb.get(), cmap=cm.hot, clim=(0.0, 0.7 * imgb.get().max()))
+        plt.imshow(imgb, cmap=cm.hot, clim=(0.0, 0.7 * imgb.max()))
 except AssertionError as error:
     print(error)
 
@@ -191,7 +195,7 @@ if Nsize != 512:
 else:
     img2 = np.single(img2)
 
-start_time = time.time()
+
 h.cleanup = False
 h.debug = False
 h.NA = 1.1
@@ -203,6 +207,7 @@ h.n = 1.33
 
 ''' Calibration cupy'''
 try:
+    start_time = time.time()
     h.calibrate(img2[140:147, :, :])
     elapsed_time = time.time() - start_time
     print(f'Calibration time: {elapsed_time:5f}s ')
@@ -227,6 +232,7 @@ start_time = time.time()
 imgouta = h.batchreconstruct(img2)
 elapsed_time = time.time() - start_time
 print(f'Batch Reconstruction time (CPU): {elapsed_time:5f}s ')
+
 
 if isPlot:
     plt.figure()